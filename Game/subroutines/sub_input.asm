5435cd99c02402bb13cec2f4ec65a26521212dc5
Brian Gulotta <briangulotta@Brians-MacBook-Pro.local>
2020-04-24 12:24:53 -0500

    animation update

--- Game/subroutines/sub_input.asm
+++ Game/subroutines/sub_input.asm
@@ -3,8 +3,6 @@
 #importonce
 
 check_input:
-    lda #$00
-    sta spritedirection
     lda #jump
     bit cia_port_a
     beq start_jump
@@ -34,8 +32,6 @@ start_jump:
 move_up:
     bit spritemovement
     beq input_exit
-    lda #up
-    sta spritedirection    
     sec 
     lda spritey
     sbc spritemovementspd
@@ -44,8 +40,6 @@ move_up:
 move_down:
     bit spritemovement
     beq input_exit
-    lda #down
-    sta spritedirection    
     clc 
     lda spritey
     adc spritemovementspd
